8c5a34e2c9d972d56eb9778c7dbb528276c46d0a
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-28 12:25:26 +0000

    calculates days until birthday

--- lib/birthday_counter.rb
+++ lib/birthday_counter.rb
@@ -1,10 +1,26 @@
+require "date"
+
 class BirthdayCounter
 
   attr_reader :birthdate
 
   def initialize(day, month)
-    @birthdate = Time.new(2000, month.to_i, day.to_i)
+    @birthdate = Date.new(Date.today.year, month.to_i, day.to_i)
 
+  end
+
+  def today?
+    now = Date.today
+    (@birthdate.day == now.day) && (@birthdate.month == now.month) 
+  end
+
+  def days_until
+    days = (@birthdate - Date.today).to_i
+    if days >= 0
+      days
+    else
+      365 + days
+    end
   end
 
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,3 +1,4 @@
+require "date"
 require "birthday_counter"
 
 describe BirthdayCounter do 
@@ -7,7 +8,28 @@ describe BirthdayCounter do
     expect(birthday.birthdate.day).to eq 4
   end
 
-  it "checks if birthday is today"
+  it "knows if birthday is today" do
+    now = Date.today
+    birthday = BirthdayCounter.new(now.day, now.month)
+    expect(birthday).to be_today
+  end
 
+  it "also knows if birthday is not today" do
+    now = Date.today
+    birthday = BirthdayCounter.new(now.day + 1, now.month)
+    expect(birthday).not_to be_today
+  end
+
+  it "calculates days until nearby birthday" do
+    allow(Date).to receive(:today).and_return(Date.new(2021, 7, 1))
+    birthday = BirthdayCounter.new("5", "7")
+    expect(birthday.days_until).to eq 4
+  end
+
+  it "calculates day until faraway birthday" do
+    allow(Date).to receive(:today).and_return(Date.new(2021, 7, 1))
+    birthday = BirthdayCounter.new("1", "6")
+    expect(birthday.days_until).to eq 335
+  end
 
 end
